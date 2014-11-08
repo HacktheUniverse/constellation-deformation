64207d3444e136064534b2f627aa87d5dc2cd55d
pselle <pamela.selle@gmail.com>
2014-11-08 14:30:00 -0500

    Heroku ready

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 var express = require('express');
 var app = express();
 var port = process.env.PORT || 3000;
-app.use(express.static('app'));
+app.use(express.static('delorean'));
 
 app.listen(port);

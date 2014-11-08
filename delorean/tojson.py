632a3110761c7e6bd311910f1055b451b9d14983
Nathaniel Lash <lash2@illinois.edu>
2014-11-08 10:32:15 -0500

    star/python

--- delorean/tojson.py
+++ delorean/tojson.py
@@ -3,16 +3,13 @@ i = 0
 with open('data/stars.text','r') as f:
 	with open('data/stars.json', 'w') as out:
 		#orion_stars = [27989, 26207, 25336, 25930, 25281, 24436, 27366, 26727, 28614, 29426, 28691, 27913, 29038, 26311, 22449, 22845, 22509, 22549, 22797, 23123]
-		for line in f:
-			
-			parts = line.split()
-			if i < 3000:
-				if len(parts) >1:
+		for line in f:			
+			parts = line.split()	
+			if len(parts) >1:
+				if float(parts[6]) < 6.5:
 					out.write('{"pos":[%s , %s, %s], "color": %s, "lum":%s, "speed":[%s,%s,%s], "hip":%s },' %(
 						parts[0],parts[1],parts[2],parts[3],parts[4], parts[12], parts[13], parts[14], parts[18].strip('HIP')
 						))
-			i+=1;
-
 	
 		
 

6ac41f2ebfccbe719a8539f2823c5fc6dd74fa7d
Nathaniel Lash <lash2@illinois.edu>
2014-11-08 05:14:00 -0500

    all to fetch

--- delorean/app.js
+++ delorean/app.js
@@ -9,6 +9,8 @@ var ExampleApplication = React.createClass({
   }
 });
 
+var camera, scene, renderer, group, particle;
+
 var start = new Date().getTime();
 
 setInterval(function() {
@@ -36,41 +38,43 @@ var particleCount = stars.length,
 function write_new_coords(star, date) {
 
 }
-var time = 0;
-
+var time = 0; //THE UNITS ARE MEGAYEARS.
 // now create the individual particles
 for (var p = 0; p < particleCount; p++) {
   var star = stars[p];
   var coords = star['pos'];
-  console.log(coords);
+  var velocities = star['speed'];
 
-  var velocities = star['speed']
-  console.log(velocities)
-
-
-  var pX = coords[0] * ,
-      pY = coords[1],
-      pZ = coords[2],
+  var pX = coords[0] + (time * velocities[0]),
+      pY = coords[1] + (time * velocities[1]),
+      pZ = coords[2] + (time * velocities[2]),
+  
       particle = new THREE.Particle(
         new THREE.Vector3(pX, pY, pZ)
       );
 
-  // add it to the geometry
+  var distance = Math.sqrt(Math.pow(pX,2) + Math.pow(pY,2) + Math.pow(pZ,2))
+  var flux = star.lum / (4*3.14 (Math.pow(distance,2)));
+  
   particles.vertices.push(particle);
 }
-console.log(particles.vertices);
+
+scene.add(particles);
+
 // create the particle system
 var particleSystem = new THREE.PointCloud(
     particles,
     pMaterial);
+
 var geometry = new THREE.BoxGeometry( 1, 1, 1 );
+
 var material = new THREE.MeshBasicMaterial( { color: 0x00ff00 } );
 
-camera.position.z = 5;
+// camera.position.z = 5;
 
 // add it to the scene
 scene.add(particleSystem);
-
+scene.add(particles)
 function render() { 
   requestAnimationFrame( render );
   renderer.render( scene, camera );

1286635f9a1d9082e77c24db43657ff7fae48b8b
Nathaniel Lash <lash2@illinois.edu>
2014-11-08 13:35:37 -0500

    blah

--- delorean/app.js
+++ delorean/app.js
@@ -19,7 +19,9 @@ function init() {
   scene = new THREE.Scene();
   scene.fog = new THREE.FogExp2(0x000000, 0.0007);
   geometry = new THREE.Geometry();
+  var orion = 
   $.getJSON('data/stars.json', function(stars) {
+
     window.star = stars[1];
 
     for (var i = 0; i < stars.length; i ++ ) {
@@ -29,6 +31,9 @@ function init() {
       vertex.x = coords[0];
       vertex.y = coords[1];
       vertex.z = coords[2];
+      vertex.x0 = coords[0];
+      vertex.y0 = coords[1];
+      vertex.z0 = coords[2];
       vertex.velocities = star.speed;
 
       geometry.vertices.push( vertex );
@@ -36,7 +41,9 @@ function init() {
     material = new THREE.PointCloudMaterial( { size: 3, sizeAttenuation:false } );
     particles = new THREE.PointCloud( geometry, material );
     scene.add( particles );
-  });
+  }).fail(function(error){
+    console.log(error)
+  })
 
   renderer = new THREE.WebGLRenderer();
   renderer.setSize( window.innerWidth, window.innerHeight );
@@ -56,27 +63,74 @@ function init() {
 
 function updateVertices(geometry) {
   geometry.vertices.forEach(function(v) {
-    v.x += (time * v.velocities[0]);
-    v.y += (time * v.velocities[1]);
-    v.z += (time * v.velocities[2]);
+    v.x = (v.x0 + time * v.velocities[0]);
+    v.y = (v.y0 + time * v.velocities[1]);
+    v.z = (v.z0 + time * v.velocities[2]);
   });
 
   geometry.verticesNeedUpdate = true;
 }
 
 var frames  = 0;
-function animate() {
 
+  
+$('input[type="range"]').rangeslider({
+
+      // Feature detection the default is `true`.
+      // Set this to `false` if you want to use
+      // the polyfill also in Browsers which support
+      // the native <input type="range"> element.
+      polyfill: true,
+
+      // Default CSS classes
+      rangeClass: 'rangeslider',
+      fillClass: 'rangeslider__fill',
+      handleClass: 'rangeslider__handle',
+
+      // Callback function
+
+      // Callback function
+      onSlide: animate
+
+      
+  });
+
+function getBaseLog(x, y) {
+    return Math.log(y) / Math.log(x);
+}
+
+function scale_time(value) {
+  //if -1 return -10E-6
+  //if 1 return 10E-6
+  //2 return 10E-5
+  //3 
+  var is_negative = time < 0;
+
+  var one_year = Math.pow(10, -6);
+  var log_input = Math.pow(10, Math.abs(value))
+
+  if (is_negative)  { log_input * -1;}
+  return one_year * log_input;
+}
+
+function animate() {
+  var value = document.getElementById("time_slider").value
+  console.log(value);
+  // console.log(value)
+  time = scale_time(value)
+  console.log(time)
   requestAnimationFrame( animate );
 
   frames = frames + 1;
   render();
   stats.update();
+  
   if(scene.children[0] && (frames % 10 == 0)) {
-    time = time + 0.01;
+  //   time = time + 0.0000001; //each frame increment a year.
     updateVertices(scene.children[0].geometry);
   }
 }
+
 
 function render() {
 

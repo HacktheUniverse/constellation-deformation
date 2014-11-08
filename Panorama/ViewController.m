75d8e6cd47044aef022b4576b7ff814a920c0105
robbykraft <robbykraft@gmail.com>
2014-11-08 00:57:40 -0500

    moving stars

--- Panorama/ViewController.m
+++ Panorama/ViewController.m
@@ -13,8 +13,9 @@
 
 @interface ViewController (){
     PanoramaView *panoramaView;
-    float *stars;
-    unsigned long numStars;
+    float *stars, *velocities;
+    unsigned int numStars;
+    NSTimer *increment;
 }
 @end
 
@@ -31,25 +32,42 @@
     [panoramaView setShowTouches:NO];
     [self setView:panoramaView];
 }
-
+//id,x,y,z,colorb_v,lum,absmag,appmag,texnum,distly,dcalc,plx,plxerr,vx,vy,vz,speed,hipnum,name
 -(void)loadStars{
     NSString *path = [[NSBundle mainBundle] pathForResource:@"visible_stars_hacky" ofType:@"csv"];
     NSMutableArray *array = [NSMutableArray arrayWithContentsOfCSVFile:path];
-//    [array removeObjectAtIndex:0];  // database has one line of header
+
     NSLog(@"%d stars loaded",(int)array.count);
-//    NSLog(@"%@",array);
-    numStars = array.count - 1;
+
+    numStars = (int)array.count - 1;
     stars = malloc(sizeof(float)*numStars*3);
+    velocities = malloc(sizeof(float)*numStars*3);
+
     for(int i = 0; i < numStars; i++){
         stars[i*3+0] = [[[array objectAtIndex:i] objectAtIndex:1] floatValue];
         stars[i*3+1] = [[[array objectAtIndex:i] objectAtIndex:2] floatValue];
         stars[i*3+2] = [[[array objectAtIndex:i] objectAtIndex:3] floatValue];
     }
     for(int i = 0; i < numStars; i++){
-        NSLog(@"X:%.3f  Y:%.3f  Z:%.3f", stars[i*3+0], stars[i*3+1], stars[i*3+2]);
+        velocities[i*3+0] = [[[array objectAtIndex:i] objectAtIndex:13] floatValue] / 1000;
+        velocities[i*3+1] = [[[array objectAtIndex:i] objectAtIndex:14] floatValue] / 1000;
+        velocities[i*3+2] = [[[array objectAtIndex:i] objectAtIndex:15] floatValue] / 1000;
     }
+    
+    
+//    for(int i = 0; i < numStars; i++){
+//        NSLog(@"X:%.3f  Y:%.3f  Z:%.3f", stars[i*3+0], stars[i*3+1], stars[i*3+2]);
+//    }
+    increment = [NSTimer scheduledTimerWithTimeInterval:1.0/60 target:self selector:@selector(incrementStars) userInfo:nil repeats:YES];
 }
 
+-(void) incrementStars{
+    for(int i = 0; i < numStars; i++){
+        stars[i*3+0] += velocities[i*3+0];
+        stars[i*3+1] += velocities[i*3+1];
+        stars[i*3+2] += velocities[i*3+2];
+    }
+}
 
 -(void) glkView:(GLKView *)view drawInRect:(CGRect)rect{
     glClearColor(0.0f, 0.0f, 0.0f, 0.0f);

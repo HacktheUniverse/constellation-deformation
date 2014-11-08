e169c171f9c2b49cf812ba9a0d9ddf091bbc8701
Nathaniel Lash <lash2@illinois.edu>
2014-11-08 06:12:07 -0500

    really truncate

--- delorean/tojson.py
+++ delorean/tojson.py
@@ -1,16 +1,17 @@
 import json
+i = 0
 with open('data/stars.text','r') as f:
 	with open('data/stars.json', 'w') as out:
 		#orion_stars = [27989, 26207, 25336, 25930, 25281, 24436, 27366, 26727, 28614, 29426, 28691, 27913, 29038, 26311, 22449, 22845, 22509, 22549, 22797, 23123]
 		for line in f:
-			i = 0
+			
 			parts = line.split()
 			if i < 3000:
 				if len(parts) >1:
 					out.write('{"pos":[%s , %s, %s], "color": %s, "lum":%s, "speed":[%s,%s,%s], "hip":%s },' %(
 						parts[0],parts[1],parts[2],parts[3],parts[4], parts[12], parts[13], parts[14], parts[18].strip('HIP')
 						))
-				i+=1;
+			i+=1;
 
 	
 		
